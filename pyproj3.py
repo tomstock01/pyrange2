45cda29e687d33c25302399f3866b550d53c4a5b
tomstock01 <tstock2@gmail.com>
2016-12-20 15:45:15 -0800

    python range() project
    basic use of python range() function

---
+++ pyproj3.py
@@ -0,0 +1,16 @@
+# Python 3.5.2
+# Tom Stock
+# Basic python skills project, using range() function
+
+list1 = [0,1,2,3,4]
+for i in range(4):
+    print(i)
+
+
+list2 = [0,1,2,3]
+for j in range(4, 0, -1):
+    print(j - 1)
+
+list3 = [1,2,3,4,5,6,7,8]
+for k in range(8, 1, -2):
+    print(k)
